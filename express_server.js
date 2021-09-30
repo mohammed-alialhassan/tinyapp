56abac28c010605abb765ba7ab274f0cd9156de4
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-30 23:34:09 +0000

    added delete function

--- express_server.js
+++ express_server.js
@@ -54,6 +54,12 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
 /*
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
